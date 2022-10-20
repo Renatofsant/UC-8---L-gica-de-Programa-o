8d64e22960816a059f1565627e5d8c954c5167aa
RENATO SANTOS <114427740+Renatofsant@users.noreply.github.com>
2022-10-19 21:36:45 -0300

    Add files via upload

---
+++ Lista de compras.js
@@ -0,0 +1,26 @@
+var listadecompras = ["Carne","Leite","Farinha de trigo","Ovos","Macarrão","Arroz","Feijão","Batata","Cenoura","Chuchu","Hamburguer","Aipim","Manteiga"]
+
+let peso = 1000
+if(peso<300){
+    console.log("Peso abaixo do desejavel")
+}else{
+    console.log("Peso ideal")
+}
+let nomecompra = "Ovos"
+if(nomecompra.length>5){
+    console.log("Mercadoria correta")
+}else{
+    console.log("Mercadoria errada")
+}
+let listadecompras = ("Carne","Leite","Hamburguer","Aipim","Manteiga")
+if(listadecompras.length<3){
+    console.log("Continuar compras")
+}else{
+    console.log("Encerrar compras")
+}
+let numerodeprodutos = 13
+if(numerodeprodutos<12){
+    console.log("Compra quase acabando")
+}else{
+    console.log("Compra encerrada, carrinho cheio")
+}
---
+++ Revisando atividade.js
@@ -0,0 +1,21 @@
+
+let teste = 80
+let folhaAZ = 20
+let prova = 90
+let media = ((teste+folhaAZ)+prova)/2
+console.log("Sua media final é",media)
+if(media >=0 && media <=69){
+    console.log("Estudante abaixo da média, estude mais um pouco, você é capaz.")
+}
+if(media >=70 && media <=80){
+    console.log("Estudante acima da média, parabéns.")
+}
+if(media >=81 && media <=99){
+        console.log("Caramba! Você é demais.")
+}
+if(media >= 100){
+    console.log("Parabéns, você é um gênio!")
+}
+
+
+
